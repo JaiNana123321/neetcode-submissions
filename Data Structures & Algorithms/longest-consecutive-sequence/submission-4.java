5f7afb314c76de5edec73b71b7099429ff614725
Jai Nana <jai.nana.rsa@gmail.com>
2026-08-07 18:33:09 -0400

    Add: longest-consecutive-sequence - submission-4

---
+++ Data Structures & Algorithms/longest-consecutive-sequence/submission-4.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int longestConsecutive(int[] nums) {
+        HashSet<Integer> hs = new HashSet<>();
+        for (int x : nums) {
+            hs.add(x);
+        }
+
+        List<Integer> possible_starts_of_seq = new ArrayList<>(nums.length);
+        for(int x : hs){
+            if(!hs.contains(x-1)){
+                possible_starts_of_seq.add(x);
+            }
+        }
+
+        int max_len = 0;
+        int len = 0;
+        
+        for(int candidate : possible_starts_of_seq){
+            while(hs.contains(len + candidate)){
+                len++;
+            }
+            if(len > max_len) max_len = len;
+            len = 0;
+        }
+
+        return max_len;
+        
+    }
+}
