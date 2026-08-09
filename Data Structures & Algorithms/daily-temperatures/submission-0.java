feea13123a9630655a0c036f8c4af83225fe9ec1
Jai Nana <jai.nana.rsa@gmail.com>
2026-08-09 18:41:46 -0400

    Add: daily-temperatures - submission-0

---
+++ Data Structures & Algorithms/daily-temperatures/submission-0.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int[] dailyTemperatures(int[] temperatures) {
+        int[] result = new int[temperatures.length];
+        Deque<Integer> stack = new ArrayDeque<>();
+
+        for(int i = 0; i < temperatures.length; i++){
+            while(!stack.isEmpty() && temperatures[stack.peek()] < temperatures[i]){
+                int j = stack.pop(); 
+                result[j] = i - j;
+            }
+            stack.push(i);
+        }
+
+        return result;
+        
+    }
+}
